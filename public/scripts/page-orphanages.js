7569b5e9d88b942307b458392849228388c70f97
Marlon Balla <marlonballa07@gmail.com>
2021-02-14 22:34:35 -0300

    :pencil: Finalization of the orphanages location pages

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,10 +1,30 @@
-const map = L.map("mapid").setView([51.505, -0.09], 13);
+//create map
+const map = L.map("mapid").setView([-21.244995, -48.8063554], 16);
 
-L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-  attribution:
-    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-}).addTo(map);
+//create and add tileLayer
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
 
-L.marker([51.5, -0.09]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+//create icon
+const icon = L.icon({
+  iconUrl: "./public/images/logo-icon.png",
+  iconSize: [58, 68],
+  iconAnchor: [29, 68],
+  popupAnchor: [170, 2],
+});
+
+//create popup overlay
+
+const popup = L.popup({
+  closeButton: false,
+  className: "map-popup",
+  minWidth: 240,
+  minHeight: 240,
+}).setContent(
+  'Teste de Nome<a href="orphanage.html?id=1" class="choose-orphanage"><img src="../public/images/arrow-white.svg"></a>'
+);
+
+//create and add maker
+L.marker([-21.244995, -48.8063554], { icon })
+  .addTo(map)
+  .bindPopup(popup)
+  .openPopup();
